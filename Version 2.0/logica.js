fbae02dd4e3a9bc68e666d61e660ccfb4b444e65
Jackson Carvalho <jack.jackspp@gmail.com>
2021-10-18 20:28:35 -0300

    up sort system

--- Version 2.0/logica.js
+++ Version 2.0/logica.js
@@ -51,6 +51,7 @@ function addDados() {
         tdPra = document.createElement('td');
         tdKm = document.createElement('td');
         tdPorkm = document.createElement('td');
+        tdPorkm.className = 'porkm';
 
         parseFloat(valor);
         parseFloat(km);
@@ -60,7 +61,7 @@ function addDados() {
         tdVal.append(`R$ ${valor}`);
         tdPra.append(`${prazo} dias`);
         tdKm.append(km);
-        tdPorkm.append(`R$ ${porKM.toFixed(2)}`)
+        tdPorkm.append(porKM.toFixed(2))
 
         tr.append(tdTr, tdVal, tdPra, tdKm, tdPorkm);
         tableMaked.appendChild(tr);
@@ -121,14 +122,55 @@ function createTable() {
         rowData.append(des);
         rowData.append(sal);
         rowData.append(pla);
-   
+
         tableMaked.prepend(rowData);
         
         document.getElementById('divTableMaked').append(tableMaked);
+
+        sortTable();
     }
 }
 
 //Função que reseta a página para reutilização
 function reset() {
     window.location.href = '';
+}
+
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+
+    table = document.getElementById('tableMaked');
+
+    switching = true;
+
+    /* Make a loop that will continue until no switching has been done: */
+    
+    while (switching) {
+
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+
+        /* Loop through all table rows (except the first, which contains table headers): */
+        
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare, one from current row and one from the next: */                        
+            x = rows[i].getElementsByClassName('porkm')[0];
+            y = rows[i + 1].getElementsByClassName('porkm')[0];
+            // Check if the two rows should switch place:
+            if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
